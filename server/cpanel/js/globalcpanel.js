f9e1919e92df980ca2516df603a371de67351cb1
Steyn Potze <steyn.potze@me.com>
2016-05-21 12:57:21 +0200

    Added some of the features from client to cpanel

--- server/cpanel/js/globalcpanel.js
+++ server/cpanel/js/globalcpanel.js
@@ -1,5 +1,16 @@
 var app = angular.module('dataskylineControlApp', ['ngRoute', 'ui.bootstrap']);
 
+/*
+  Set the page title to "DataSkyline control panel" to make sure a title is present
+  The page title should be changed to something more specific once a page is loaded.
+*/
+app.run(function($rootScope) {
+    $rootScope.title = "DataSkyline control panel";
+});
+
+/*
+  Show the correct page on screen.
+*/
 app.config(function($routeProvider) {
   $routeProvider.
   when('/modules', {
@@ -20,4 +31,17 @@ app.controller('NavigationCtrl', function($scope, $location) {
   $scope.isActive = function(page) {
     return page === $location.path();
   };
+});
+
+/*
+  Keeps a connection to the DataSkyline websocket server and
+  provides functions and callbacks for interacting with this server.
+*/
+app.factory('DataSkylineWS', function() {
+    var functions = {};
+    // TODO: Connect to WS
+    // TODO: Keep a list of callbacks to call on message received
+    // TODO: "subscribe" and "unsubscribe" for callbacks
+    // TODO: Provide methods for sending commands to WS
+    return functions;
 });
