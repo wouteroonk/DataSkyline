169545d1679501347a28aa3ed0db0eef9648189a
Wouter Oonk <oonkwouter@gmail.com>
2016-05-25 14:25:05 +0200

    Bounce and color changed

--- server/touchpanel/js/touch.js
+++ server/touchpanel/js/touch.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
-    var j=0;
-    while(j<2){
-        $("#container").append("<div id='button_"+ j +"' class='button'></div>");
-        j++;
-    }
 
-    $(".button").throwable({
-        gravity:{x:0,y:0.001},
-        bounce:0.8,
-        impulse:{
-            f:100,
-            p:{x:3,y:3}
-        },
-        shape: "circle",
-    });
+  var colors = ["bgBlue","bgGreen","bgPink","bgOrange","bgPurple"];
+
+  var j=0;
+  while(j<4){
+    $("#container").append("<div id='button_"+ j +"' class='button " + colors[j % colors.length] + "'>Button " + j + "</div>");
+    j++;
+  }
+
+  $(".button").throwable({
+    gravity:{x:0,y:0},
+    bounce:1,
+    impulse:{
+      f:100,
+      p:{x:3,y:3}
+    },
+    shape: "circle",
+  });
 });
