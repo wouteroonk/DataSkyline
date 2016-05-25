0e7559a2b57be4e080d9370b0a892341efbe71fd
Dennis Roelvink <dennis.roelvink@planet.nl>
2016-05-25 09:27:32 +0200

    Update

--- server/tempfile.js
+++ server/tempfile.js
@@ -2,31 +2,45 @@ var fs = require("fs"); //Access local filesystem
 
 
 module.exports = {
-    readModules : function() {
+    readModules : function(callback) {
+        var listing = [];
         fs.readdir("modules",function(err,list) {
             if(err) {
                 console.log("Error");
             } else {
-                for(i = 0 ; i < list.length; i++) {
+                for(var i = 0 ; i < list.length; i++) {
                     var item = list[i];
                     if(fs.lstatSync("modules/"+item).isDirectory()){
-                        console.log(item);
+                        listing.push(item);
                     }
                 }
             }
+            return callback(listing);
         });
     },
 
-    readAllClients : function() {
-        var clients = require("./assets/clients.json");
-        for(i = 0; i < clients.length; i++){
-            console.log(clients[i].Name + " With IP: " + clients[i].Address);
+    readAllClients : function(callback) {
+        var info = require("./assets/info.json");
+        var names = [];
+        var addresses = [];
+        console.log("Amount of clients: " + info.client_amount);
+        for(var i = 0; i < info.client_amount; i++){
+            names.push(info.clients[i].name);
+            addresses.push(info.clients[i].address);
+            //.log(info.clients[i].name + " With IP: " + info.clients[i].address);
         }
+        return callback(names,addresses);
     }
-}
+};
 
 var temp = require("./tempfile.js");
 
-temp.readAllClients();
+temp.readAllClients(function(names,addresses){
+    console.dir(names);
+    console.dir(addresses);
+});
 console.log("---");
-temp.readModules();
+
+temp.readModules(function(list) {
+    console.dir(list);
+});
