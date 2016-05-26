2ce9afff1055c6294d54eb9d4be16b92587e6156
Wouter Oonk <oonkwouter@gmail.com>
2016-05-26 10:23:03 +0200

    Try to drag the circles with draggables and droppables

--- server/touchpanel/js/touch.js
+++ server/touchpanel/js/touch.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
 
   var colors = ["bgBlue","bgGreen","bgPink","bgOrange","bgPurple"];
 
+  function drag(e) {
+      e.dataTransfer.setData("text", e.target.dataset.id);
+  }
+
   var j=0;
-  while(j<4){
-    $("#container").append("<div id='button_"+ j +"' class='button " + colors[j % colors.length] + "'>Button " + j + "</div>");
+  while(j<1){
+    $("#container").append("<div draggable='true' ondragstart='alert(\"test\")' id='button_"+ j +"' class='button " + colors[j % colors.length] + "'>Button " + j + "</div>");
     j++;
   }
 
@@ -15,6 +19,21 @@ $(document).ready(function () {
       f:100,
       p:{x:3,y:3}
     },
-    shape: "circle",
+    shape: "circle"
   });
+
+  $( ".button" ).draggable();
+
+  $( "#area" ).droppable({
+     drop: function( event, ui ) {
+       console.log("dropped")
+     }
+   });});
+
+$(document).on("inarea",function (event,data){
+    $("#area").css("background-color","blue");
+});
+
+$(document).on("outarea",function (event,data){
+    $("#area").css("background-color","black");
 });
