4336844da727136afaeec5c2ea37c94598428927
Steyn Potze <steyn.potze@me.com>
2016-05-27 09:20:29 +0200

    Implemented HTML, removed evil eval method

--- client/js/ctrl/dscmsLiveSkylineCtrl.js
+++ client/js/ctrl/dscmsLiveSkylineCtrl.js
@@ -13,56 +13,65 @@
       - VIEWS are send to clients and are basically mini websites.
 */
 dscms.app.controller('dscmsLiveSkylineCtrl', function($scope, $rootScope, $compile, dscmsWebSocket) {
-  // TODO: Get views to show from WS
-  // TODO: Instantiate views
-  // TODO: Listen for updates from WS
-  // Subscribe to websocket updates
-  dscmsWebSocket.subscribe(function(message) {
-    var commands = message.data.split(' ');
-    // Respond to various messages from server
-    switch (commands.shift()) {
+    // TODO: Get views to show from WS
+    // TODO: Instantiate views
+    // TODO: Listen for updates from WS
+    // Subscribe to websocket updates
+    dscmsWebSocket.subscribe(function(message) {
+        var commands = message.data.split(' ');
+        // Respond to various messages from server
+        switch (commands.shift()) {
 
-      // The windowinfo message contains information about the windows that should be shown,
-      // such as:
-      //  * Size, shape and position
-      //  * Javascript file
-      //  * HTML layout files
-      case "windowinfo":
-        // TODO: Fill screen, instantiate views
-        var returnedJSON;
-        try {
-          returnedJSON = JSON.parse(message.data.substring(message.data.indexOf(' ') + 1));
-        } catch (e) {
-          console.error("Server did not return JSON in windowinfo message: " + message.data);
-          console.dir(message);
-          return;
+            // The windowinfo message contains information about the windows that should be shown,
+            // such as:
+            //  * Size, shape and position
+            //  * Javascript file
+            //  * HTML layout files
+            case "windowinfo":
+                // TODO: Fill screen, instantiate views
+                var returnedJSON;
+                try {
+                    returnedJSON = JSON.parse(message.data.substring(message.data.indexOf(' ') + 1));
+                } catch (e) {
+                    console.error("Server did not return JSON in windowinfo message: " + message.data);
+                    console.dir(message);
+                    return;
+                }
+                initWindow(returnedJSON);
+                break;
+
+                // The test message is used for testing purposes and should be deleted.
+            case "test":
+                console.log(message.data.substring(message.data.indexOf(' ') + 1));
+                break;
+
+            default:
+                console.error("Unknown message received: " + message.data);
+                console.dir(message);
         }
-        initWindow(returnedJSON);
-        break;
+    });
 
-        // The test message is used for testing purposes and should be deleted.
-      case "test":
-        console.log(message.data.substring(message.data.indexOf(' ') + 1));
-        break;
+    // Get own IP to send to server
+    dscmsWebSocket.requestOwnLocalIP(function(ip) {
+        // Ask server to send window info, handled by callback defined above
+        dscmsWebSocket.requestWindowsForIP(ip);
+    });
 
-      default:
-        console.error("Unknown message received: " + message.data);
-        console.dir(message);
-    }
-  });
+    // Init window loads views into the window and initiates their JS files
+    var initWindow = function(data) {
+        // Update the title because we now know which screen we are
+        $rootScope.title = "DataSkyline - " + data.screenName;
 
-  // Get own IP to send to server
-  dscmsWebSocket.requestOwnLocalIP(function(ip) {
-    // Ask server to send window info, handled by callback defined above
-    dscmsWebSocket.requestWindowsForIP(ip);
-  });
-
-  var initWindow = function(data) {
-    $rootScope.title = "DataSkyline - " + data.screenName;
-    $scope.views = data.views;
-    for ($scope.viewIt = 0; i < $scope.views; $scope.viewIt++) {
-      // TODO: Create views, waiting for actual data
-      $('#dscms-modules').append($compile("<dscms-view dscms-data-object='views[viewIt]'></dscms-view>")($scope));
-    }
-  };
+        // Loop through views to initiate them
+        $scope.views = data.views;
+        for (var i = 0; i < $scope.views.length; i++) {
+            // Further initialisation is handled by dscmsViewCtrl
+            $scope.tempID = "dscms-view-" + i;
+            $scope.tempDataBundle = {
+                "id": $scope.tempID,
+                "view": $scope.views[i]
+            };
+            $('#dscms-modules').append($compile("<dscms-view id='{{tempID}}' dscms-data-object='tempDataBundle'>test</dscms-view>")($scope));
+        }
+    };
 });
