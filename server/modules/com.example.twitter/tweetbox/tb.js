a925d4b26b9a1986d05d316088cbf4e4aed8f7c1
Steyn Potze <steyn.potze@me.com>
2016-05-29 20:49:24 +0200

    Replaced demo tweets with politically correct ones

--- server/modules/com.example.twitter/tweetbox/tb.js
+++ server/modules/com.example.twitter/tweetbox/tb.js
@@ -1,4 +1,4 @@
 console.log("Hi! I am the script for the tweetbox view with ID " + $scope.dscmsViewId + " from the Twitter module!");
-$('#' + DSCMSViewTools.myWindows['Avatar Window'] + " img").attr('src', ($scope.dscmsViewId % 2) ? 'https://pbs.twimg.com/profile_images/444885341661437953/osfNSOEm.jpeg' : 'https://pbs.twimg.com/profile_images/1980294624/DJT_Headshot_V2.jpg');
-$('#' + DSCMSViewTools.myWindows['Text Window'] + " h1").text(($scope.dscmsViewId % 2) ? '@Llama89:' : '@realDonaldTrump:');
-$('#' + DSCMSViewTools.myWindows['Text Window'] + " p").text(($scope.dscmsViewId % 2) ? 'Oh wow! I figured out how to tweet! #comingforyouhumans' : 'I love China!');
+$('#' + DSCMSViewTools.myWindows['Avatar Window'] + " img").attr('src', ($scope.dscmsViewId % 2) ? 'http://www.clearvoicebranding.com/wp-content/uploads/2012/09/iStock_000015424718XSmall.jpg' : 'http://pre12.deviantart.net/b384/th/pre/i/2013/175/f/6/male_stock_247_by_birdsistersstock-d5xmnrq.jpg');
+$('#' + DSCMSViewTools.myWindows['Text Window'] + " h1").text(($scope.dscmsViewId % 2) ? '@AbigailA:' : '@AdamW:');
+$('#' + DSCMSViewTools.myWindows['Text Window'] + " p").text(($scope.dscmsViewId % 2) ? 'Just finished Friends... What should I watch next? #helpme' : 'BIG news! I am getting married in two months!');
