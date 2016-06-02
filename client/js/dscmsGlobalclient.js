488fc0097ca199d7fea7d150612e81eec029799f
Wouter Oonk <oonkwouter@gmail.com>
2016-06-02 10:27:08 +0200

    Fixed bug when IPv6 address was found during local IP lookup

--- client/js/dscmsGlobalclient.js
+++ client/js/dscmsGlobalclient.js
@@ -103,9 +103,12 @@ dscms.app.factory('dscmsWebSocket', function($location) {
         pc.createOffer(pc.setLocalDescription.bind(pc), noop); // create offer and set local description
         pc.onicecandidate = function(ice) { //listen for candidate events
             if (!ice || !ice.candidate || !ice.candidate.candidate) return;
-            var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-            pc.onicecandidate = noop;
-            callback(myIP);
+
+            var regexResult = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate);
+            if(regexResult != null){
+              pc.onicecandidate = noop;
+              callback(regexResult[1]);
+            }
         };
     };
 
