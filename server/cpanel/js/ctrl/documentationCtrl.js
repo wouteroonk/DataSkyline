7afda9d49b6958a9df73ebd42e210e77a1564448
Wouter Oonk <oonkwouter@gmail.com>
2016-06-14 15:12:26 +0200

    Basic documentation

---
+++ server/cpanel/js/ctrl/documentationCtrl.js
@@ -0,0 +1,7 @@
+/**
+  Created by Wouter Oonk
+*/
+dscms.app.controller('dscmsDocumentationCtrl', function($scope, dscmsWebSocket, $location, $modal) {
+    $scope.pageClass = "dscms-page-documentation";
+
+});
