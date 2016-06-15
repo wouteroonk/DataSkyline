bcf7a5b31b22e800a2fc683236a333d501924261
Steyn Potze <steyn.potze@me.com>
2016-06-15 23:02:23 +0200

    Started typing docs

--- server/cpanel/js/ctrl/documentationCtrl.js
+++ server/cpanel/js/ctrl/documentationCtrl.js
@@ -3,79 +3,99 @@
 */
 
 // $rootScope, $location, $anchorScroll, $routeParams
-dscms.app.controller('dscmsDocumentationCtrl', function($scope, $location, $anchorScroll, $routeParams, dscmsWebSocket) {
-    $scope.pageClass = "dscms-page-documentation";
+dscms.app.controller('dscmsDocumentationCtrl', function($scope, $location, $anchorScroll, $routeParams, $timeout, dscmsWebSocket) {
+  $scope.pageClass = "dscms-page-documentation";
 
-    $scope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-        // if ($location.hash()) {
-//         console.log($location.hash());
-//         // $location.hash("dscms-themes");
-//
-// console.dir($location);
+  $scope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+    // if ($location.hash()) {
+    //         console.log($location.hash());
+    //         // $location.hash("dscms-themes");
+    //
+    // console.dir($location);
 
-      $scope.gotoId($location.hash());
+    $scope.gotoId($location.hash());
 
-        // $anchorScroll();
-        // }
+    // $anchorScroll();
+    // }
+  });
+
+  $(function() {
+    var currentHash = $location.hash();
+    var currentWait = null;
+    $(document).scroll(function() {
+      $('.anchor').each(function() {
+        var top = window.pageYOffset;
+        var distance = top - $(this).offset().top;
+        var hash = $(this).attr('id');
+        // 30 is an arbitrary padding choice,
+        // if you want a precise check then use distance===0
+        if (distance < 30 && distance > -30 && currentHash != hash) {
+          $timeout.cancel(currentWait);
+          currentWait = $timeout(function() {
+            $location.hash(hash);
+          }, 0);
+          console.log(hash);
+          currentHash = hash;
+        }
+      });
     });
+  });
 
 
+  $scope.gotoId = function(eID) {
 
+    // This scrolling function
+    // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
 
-    $scope.gotoId = function(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+      scrollTo(0, stopY);
+      return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+      for (var i = startY; i < stopY; i += step) {
+        setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+        leapY += step;
+        if (leapY > stopY) leapY = stopY;
+        timer++;
+      }
+      return;
+    }
+    for (var j = startY; j > stopY; j -= step) {
+      setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+      leapY -= step;
+      if (leapY < stopY) leapY = stopY;
+      timer++;
+    }
 
-        // This scrolling function
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+    function currentYPosition() {
+      // Firefox, Chrome, Opera, Safari
+      if (self.pageYOffset) return self.pageYOffset;
+      // Internet Explorer 6 - standards mode
+      if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+      // Internet Explorer 6, 7 and 8
+      if (document.body.scrollTop) return document.body.scrollTop;
+      return 0;
+    }
 
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY);
-            return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for (var i = startY; i < stopY; i += step) {
-                setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
-                leapY += step;
-                if (leapY > stopY) leapY = stopY;
-                timer++;
-            }
-            return;
-        }
-        for (var j = startY; j > stopY; j -= step) {
-            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
-            leapY -= step;
-            if (leapY < stopY) leapY = stopY;
-            timer++;
-        }
+    function elmYPosition(eID) {
+      var elm = document.getElementById(eID);
+      var y = elm.offsetTop;
+      var node = elm;
+      while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+      }
+      return y;
+    }
 
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
-
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            }
-            return y;
-        }
-
-    };
+  };
 });
