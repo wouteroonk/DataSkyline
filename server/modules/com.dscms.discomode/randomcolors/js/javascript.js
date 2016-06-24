88eba6b11aeecfdcd3f93f4f5d4f367a24802e5c
Dennis Roelvink <dennis.roelvink@planet.nl>
2016-06-24 13:19:17 +0200

    Speed configuration posible now in Disco module

--- server/modules/com.dscms.discomode/randomcolors/js/javascript.js
+++ server/modules/com.dscms.discomode/randomcolors/js/javascript.js
@@ -1,11 +1,21 @@
 $scope.discoBG = {"background-color":"rgb(15, 102, 102)"};
+$scope.speed = 80;
 
 DSCMSView.run = function() {
+	var speed = $.grep(DSCMSViewTools.myConfig.configItems, function(item) {
+        return item.key === "speed";
+    })[0].value;
+	
+	if(speed !== "") {
+		$scope.speed = parseInt(speed);
+	}
+	
 	setInterval(function() {
 		var newrgb = getRandomRGB();
 		$scope.discoBG = {"background-color":newrgb};
 		$scope.$apply();
-	},80);
+	},$scope.speed);
+	
 }
 function getRandomRGB() {
 	var val1 = Math.floor((Math.random() * 255) + 1);
